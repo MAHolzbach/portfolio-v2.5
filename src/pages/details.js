e3c8c4420db93df8c305cb172b54537e67bebdf0
Mike Holzbach <maholzbach@gmail.com>
2019-08-10 10:13:32 -0400

    Switching over to graphql and json files

---
+++ src/pages/details.js
@@ -0,0 +1,31 @@
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Layout from "../components/layout"
+
+// const Details = ({ data }) => {
+//   console.log("data :", data)
+//   return (
+//     <Layout>
+//       <div>Hello data</div>
+//     </Layout>
+//   )
+// }
+
+// export default Details
+
+// export const query = graphql`
+//   query {
+//     allPagesJson {
+//       edges {
+//         node {
+//           detailsTitle
+//           detailsResults
+//           detailsSubtitle
+//           detailsTask
+//           detailsTeam
+//           tools
+//         }
+//       }
+//     }
+//   }
+// `
